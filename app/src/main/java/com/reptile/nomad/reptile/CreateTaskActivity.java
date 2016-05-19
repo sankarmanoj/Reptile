8ec4c023422ae8edc40debe38566f70e910d6bfd
Sankar Manoj <me@sankar-manoj.com>
2016-05-19 15:47:45 +0530

    Fix Conflict

--- app/src/main/java/com/reptile/nomad/reptile/CreateTaskActivity.java
+++ app/src/main/java/com/reptile/nomad/reptile/CreateTaskActivity.java
@@ -1,5 +1,7 @@
 package com.reptile.nomad.reptile;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
@@ -7,15 +9,43 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class CreateTaskActivity extends AppCompatActivity {
     Toolbar toolbar ;
-
+    Activity mActivity;
+    @Bind(R.id.createTaskButton)
+    Button createTaskButton;
+    @Bind(R.id.createTaskStringEditText)
+    EditText createTaskStringEditText;
+    @Bind(R.id.createTaskDeadlineTimeTextView)
+    TextView createTaskDeadlineTimeTextView;
+    @Bind(R.id.createTaskDeadlineDateTextView)
+    TextView createTaskDeadlineDateTextView;
+    Calendar deadline;
+    Boolean m24HourView = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+        ButterKnife.bind(this);
+        mActivity = this;
+        deadline = Calendar.getInstance();
         toolbar = (Toolbar) findViewById(R.id.createTaskToolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -28,8 +58,48 @@ public class CreateTaskActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        final Calendar initialDeadLine = Calendar.getInstance();
+        createTaskDeadlineDateTextView.setText(new SimpleDateFormat("E , d MMM yy", Locale.UK).format(initialDeadLine.getTime()));
+        initialDeadLine.add(Calendar.HOUR,2);
+        createTaskDeadlineTimeTextView.setText(new SimpleDateFormat("h:m a",Locale.UK).format(initialDeadLine.getTime()));
+        createTaskDeadlineTimeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog mTimePicker = new TimePickerDialog(mActivity, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        deadline.set(Calendar.MINUTE,minute);
+                        deadline.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        createTaskDeadlineTimeTextView.setText(new SimpleDateFormat("h:m a",Locale.UK).format(deadline.getTime()));
+                    }
+                },initialDeadLine.get(Calendar.HOUR),initialDeadLine.get(Calendar.MINUTE),m24HourView);
+                mTimePicker.show();
+            }
+        });
+        createTaskDeadlineDateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog mDatePicker = new DatePickerDialog(mActivity, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        deadline.set(Calendar.YEAR,year);
+                        deadline.set(Calendar.MONTH,monthOfYear);
+                        deadline.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                        createTaskDeadlineDateTextView.setText(new SimpleDateFormat("E , d MMM yy", Locale.UK).format(deadline.getTime()));
+                    }
+                },initialDeadLine.get(Calendar.YEAR),initialDeadLine.get(Calendar.MONTH),initialDeadLine.get(Calendar.DAY_OF_MONTH));
+                mDatePicker.show();
+            }
+        });
 
+        createTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/reptile/nomad/reptile/Models/Task.java
+++ app/src/main/java/com/reptile/nomad/reptile/Models/Task.java
@@ -5,43 +5,31 @@ import android.support.annotation.Nullable;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Date;
+import java.util.Calendar;
 import java.util.List;
 
 /**
  * Created by nomad on 11/5/16.
  */
 public class Task {
-
-
     public enum Status { ACTIVE , DONE, MISSED }
-//    public User creator;
-    public int userId;
-    public String username;
+    public User creator;
     private String taskString;
-    public int id;
+    public long id;
     private int likes;
-    public int comments;
-//    private List<Comment> comments;
-    private String deadline;
-    private String created;
+    private List<Comment> comments;
+    private Calendar deadline;
+    private Calendar created;
     private Status status;
-//    private List<User> visibleTo;
+    private List<User> visibleTo;
 
 
-    public Task(){
-
-    }
-    public Task(int userId, String username, String taskString, int id, int likes, int comments, String deadline, String created){
-        this.userId = userId;
-        this.username = username;
-        this.taskString = taskString;
-        this.id = id;
-        this.likes = likes;
-        this.comments = comments;
-        this.deadline = deadline;
+    public Task(User creator, String taskString,    Calendar created, Calendar deadline){
         this.created = created;
-        this.status = Status.ACTIVE;
+        this.deadline = deadline;
+        this.taskString = taskString;
+        this.status = status;
+        this.creator = creator;
     }
 
     public JSONObject createdTask()
@@ -50,7 +38,7 @@ public class Task {
         try
         {
             toSend.put("created",created);
-//            toSend.put("creator",creator.getId());
+            toSend.put("creator",creator.getId());
             toSend.put("taskstring",taskString);
             toSend.put("deadline",deadline);
         }
@@ -74,7 +62,7 @@ public class Task {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -82,31 +70,8 @@ public class Task {
         return likes;
     }
 
-    public String getUsername(){return username; }
-
     public void setLikes(int likes) {
         this.likes = likes;
-    }
-
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setCreated(String created) {
-        this.created = created;
-    }
-
-    public void setDeadline(String deadline) {
-        this.deadline = deadline;
-    }
-
-    public void setComments(int comments) {
-        this.comments = comments;
     }
 
 
